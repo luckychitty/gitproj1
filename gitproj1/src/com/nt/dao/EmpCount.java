d816c21169f0e66a043aa599b841999b2d3ea66d
TL <lenovo@192.168.0.11>
2019-04-01 13:07:05 +0530

    commit by madhu

--- gitproj1/src/com/nt/dao/EmpCount.java
+++ gitproj1/src/com/nt/dao/EmpCount.java
@@ -7,11 +7,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class EmpCount {
-	private String query="select count(*) from emp";
-	Connection con=null;{
+	private String query="SELECT COUNT(*) FROM EMP";
+	Connection con=null;
+	
+	public  void getEmpCount() throws ClassNotFoundException
+	
+	{
 
 	try {
-con=DriverManager.getConnection("jdbc:oracle:thin:@localhost@1521:orcl","system","manager");
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger");
 PreparedStatement ps=con.prepareStatement(query);
 ResultSet rs=ps.executeQuery();
 rs.next();
--- gitproj1/src/com/nt/test/EmpCountTest.java
+++ gitproj1/src/com/nt/test/EmpCountTest.java
@@ -1,9 +1,14 @@
 package com.nt.test;
 
+import com.nt.dao.EmpCount;
+
 public class EmpCountTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 		// TODO Auto-generated method stub
+		
+		EmpCount ec=new EmpCount();
+		ec.getEmpCount();
 
 	}
 
